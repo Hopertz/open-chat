e6abf5a0ab4e4a17a8187224f1048b68b3491b9f
Hopertz <lugano.ngulwa@gmail.com>
2023-08-16 16:50:05 +0300

    Update base url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client"
 
-const socket = socketIO.connect("http://localhost:8000/")
+const socket = socketIO.connect("https://api.chat.hopertz.me/")
 
 
 function App() {
